647175464da34fca4763b971827650dc1b74369b
MFRHoldings <rick@mfrholdings.com>
2021-05-10 17:48:36 -0400

    Import/Export buttons added. Maatwebsite/Laravel Excel package installed

--- resources/views/components/studentroster/students-header.blade.php
+++ resources/views/components/studentroster/students-header.blade.php
@@ -101,7 +101,35 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    Export
+                    Csv
+                </option>
+                <option value="export">
+                    <!-- Heroicon name: cog -->
+                    <svg class="text-gray-400 group-hover:text-gray-400 h-5 w-5"
+                         xmlns="http://www.w3.org/2000/svg"
+                         fill="none"
+                         viewBox="0 0 20 20"
+                         stroke="currentColor"
+                         aria-hidden="true"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    Xls
+                </option>
+                <option value="export">
+                    <!-- Heroicon name: cog -->
+                    <svg class="text-gray-400 group-hover:text-gray-400 h-5 w-5"
+                         xmlns="http://www.w3.org/2000/svg"
+                         fill="none"
+                         viewBox="0 0 20 20"
+                         stroke="currentColor"
+                         aria-hidden="true"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    Pdf
                 </option>
             </select>
         </div>
@@ -195,14 +223,46 @@
             <!-- IMPORT/EXPORT -->
             @if($showimportexport)
                 <div class="flex justify-evenly mt-2 bg-gray-200 py-2">
-                    <div>
+                    <!-- <div>
                         <input type="radio" name="importexport" id="import" value="import" />
                         <label>Import</label>
                     </div>
                     <div>
                         <input type="radio" name="importexport" id="export" value="export" />
                         <label>Export</label>
-                    </div>
+                    </div> -->
+                    <button
+                        class="font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 bg-yellow-100 disabled:opacity-50 disabled:cursor-not-allowed"
+                        type="button"
+                        wire:click="export('csv')"
+                        wire:loading.attr="disabled"
+                    >
+                        IMPORT
+                    </button>
+                    <button
+                        class="font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 bg-yellow-200 disabled:opacity-50 disabled:cursor-not-allowed"
+                        type="button"
+                        wire:click="export('csv')"
+                        wire:loading.attr="disabled"
+                    >
+                        CSV
+                    </button>
+                    <button
+                        class="font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 bg-yellow-200 disabled:opacity-50 disabled:cursor-not-allowed"
+                        type="button"
+                        wire:click="export('xlsx')"
+                        wire:loading.attr="disabled"
+                    >
+                        XLS
+                    </button>
+                    <button
+                        class="font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 bg-yellow-200 disabled:opacity-50 disabled:cursor-not-allowed"
+                        type="button"
+                        wire:click="export('pdf')"
+                        wire:loading.attr="disabled"
+                    >
+                        PDF
+                    </button>
                 </div>
             @endif
 
