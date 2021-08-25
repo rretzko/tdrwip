20215147f9bf56e45e0414d852b25be67940fa82
MFRHoldings <rick@mfrholdings.com>
2021-08-25 15:13:39 -0400

    Add URL::forceSchema("https") to routes/web.php, with dependency

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Registrants\FileapprovalController;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
