a301369815f959d58ba1ae0576169616ad433e7c
MFRHoldings <rick@mfrholdings.com>
2021-08-27 13:37:15 -0400

    Add leading "/" to css and js files

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,13 +13,13 @@
         <!-- Styles -->
         <!-- {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}} -->
         <!-- {{-- link rel="stylesheet" href="{{ asset('css/tdr.css') }}"> --}} -->
-        <link rel="stylesheet" href="css/app.css">
-        <link rel="stylesheet" href="css/tdr.css">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/tdr.css">
 
 
         <!-- Scripts -->
         <!-- {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}} -->
-        <script src="js/app.js" defer ></script>
+        <script src="/js/app.js" defer ></script>
     </head>
     <body>
         <x-site-header />
