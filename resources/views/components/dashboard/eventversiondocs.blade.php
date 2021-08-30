e2c84b8b423393902dcc1e370c5e2c9306af5da7
MFRHoldings <rick@mfrholdings.com>
2021-08-30 07:56:16 -0400

    Fix pdf assets links on eventversiondocs.blade.php

--- resources/views/components/dashboard/eventversiondocs.blade.php
+++ resources/views/components/dashboard/eventversiondocs.blade.php
@@ -10,12 +10,12 @@
         <subheader>2021-22 NJ All-State Chorus</subheader>
         <ul class="ml-8 list-disc">
             <li>
-                <a class="text-blue-700" href="assets/docs/20210611_Final_PitchFiles_Conductors.pdf" target="_BLANK">
+                <a class="text-blue-700" href="/assets/docs/20210611_Final_PitchFiles_Conductors.pdf" target="_BLANK">
                     Pitch Files
                 </a>
             </li>
             <li>
-                <a class="text-blue-700" href="assets/docs/20210609_Final_Welcome.pdf" target="_BLANK">
+                <a class="text-blue-700" href="/assets/docs/20210609_Final_Welcome.pdf" target="_BLANK">
                     Welcome email
                 </a>
             </li>
