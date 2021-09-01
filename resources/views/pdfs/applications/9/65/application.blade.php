fcfb941d8233e2b8f404cedac832e834e7370a13
MFRHoldings <rick@mfrholdings.com>
2021-09-01 13:46:39 -0400

    Hardcode njmea image on application.blade.php

--- resources/views/pdfs/applications/9/65/application.blade.php
+++ resources/views/pdfs/applications/9/65/application.blade.php
@@ -37,7 +37,7 @@
 <table>
     <tr>
         <td style="width: 10%;">
-            <img src="assets\images\njmea_logo_state.jpg" alt="NJMEA Logo"/>
+            <img src="https://thedirectorsroom.com/assets/images/njmea_logo_state.jpg" alt="NJMEA Logo"/>
         </td>
         <td style="width: 72%;">
             <div style="font-weight: bold; text-align: center">{{ $registrant->student->person->user->schools->first()->name }}</div>
