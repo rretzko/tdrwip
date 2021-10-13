ad7a5e07346dd1e18e598be2bea992b60ca8daa5
Retzko <rick@mfrholdings.com>
2021-10-12 21:57:16 -0400

    Update CJMEA Obligations

--- resources/views/components/obligations/1/70/obligations.blade.php
+++ resources/views/components/obligations/1/70/obligations.blade.php
@@ -1,71 +1,76 @@
 <ul>
     <li>
-        All Choral Directors sending student to the CJMEA REGION II CHORUS auditions must be present the entire day of
-        auditions.  A sign-in and sign-out sheet will verify attendance for the day.  Failure to comply with this
-        ruling will result in your accepted students being disqualified.
+        I understand that I must submit all signed student applications, a Teacher Estimate Form with a
+        copy of my NAfME card affixed to the back and a check made payable to CJMEA by the postmark
+        deadline of November 23, 2021.
     </li>
 
     <li>
-        Please email the Registration Manager
-        (Barbara Retzko: <a class="text-blue-700" href="mailto:barbararetzko@hotmail.com?subject=CJMEA Substitute&body=Hi Barbara -" >barbararetzko@hotmail.com</a>
-        if you need to send a substitute to fulfill your director duty.  The substitute must be a NAfME member and proof
-        of that current membership must be emailed to the Registration Manager BEFORE the day of auditions. STUDENT
-        TEACHERS AND PARENTS ARE NOT ACCEPTABLE SUBSTITUTES.
+        I understand that I must attend a Director’s meeting at 8a on the morning of the auditions – Saturday
+        December 11, 2021 (Snow Date – Saturday December 18, 2021 – 8a).
     </li>
 
     <li>
-        In the case of  an emergency, it is the Director's responsibility to see that a qualified NAfME member is present
-        at the 8:00 a.m. Director's meeting and that the Registration Manager has prior knowledge of this situation.
-        (A "qualified" NAfME member is one who currently teaches in the same area of specialization as the sponsoring
-        teacher.)
+        I understand that I must be present for the entire day of the auditions. A sign-in and sign-out sheet
+        will verify attendance for the day. If I fail to comply with this ruling, my accepted students will be
+        disqualified.
     </li>
 
     <li>
-        If you have students accepted into either the MIXED CHORUS or TREBLE CHORUS, you have a second responsibility
-        to assist with a rehearsal or at the concert.  If you have students accepted into both ensembles, you serve
-        two total duties: once at auditions and once upon student acceptance.
+        I understand that if, for any reason, I am unable to serve at the auditions, I will provide a substitute
+        who is a current member of NAfME and I will provide a copy of their current NAfME card to the
+        Registration Manager
     </li>
 
     <li>
-        On the day of auditions, teachers must sign for their second Director's Duty in the event their students are
-        accepted for participation.  Your choices of second duties include:
-        <ul>
-            <li>Rehearsal assistant at ONE rehearsal for either MIXED or TREBLE choir.</li>
-            <li>Provide assistance for the entire day of the concert.</li>
-            <li>Serve as a host school for rehearsals.</li>
-        </ul>
+        I understand that if I have students accepted into either the REGION II MIXED CHORUS or
+        TREBLE CHORUS, I have a second responsibility to assist with a rehearsal or at the concert. If I
+        have students accepted into both ensembles, I serve two total duties: once at auditions and once upon
+        student acceptance. The choices of a second duty includes rehearsal assistant at ONE rehearsal for
+        either MIXED or TREBLE CHORUS, providing assistance for the entire day of the concert or
+        serving as a host school for a rehearsal.
     </li>
 
     <li>
-        Directors who serve a Choral and/or Band/Orchestra Directors in their high school may choose to serve either
-        the CJMEA Chorus or the CJMEA Band or Orchestra.
+        I will sign up for this second Director’s Duty on the day of auditions.
     </li>
 
     <li>
-        One teacher may not sign applications and fulfill judging obligation for an entire district unless he/she is the
-        only teacher in that district.  YOU SIGN FOR THE STUDENTS YOU TEACH.
+        I understand that if I am serving as both Choral and/or Band/Orchestra Director in my school, I may
+        choose to serve either the CJMEA Chorus or the CJMEA Band or Orchestra.
     </li>
 
     <li>
-        Teachers failing to fulfill their responsibility to the 2021 MIXED CHORUS or TREBLE CHORUS will forfeit their
-        student participation the following year.
+        <div class="font-bold underline">
+            I will let the Registration Manager
+            (<a class="text-blue-700" href="mailto:barbararetzko@hotmail.com?subject=CJMEA Obligations&body=Hi Barbara - ">Barbararetzko@hotmail.com</a>)
+            know if I will be serving the instrumental auditions and not the choral auditions!
+        </div>
+    </li>
+    <li>
+        I understand that if I fail to fulfill the responsibilities to the 2022 REGION II MIXED CHORUS or
+        TREBLE CHORUS, my student participation will be forfeited the following school year.
     </li>
 </ul>
 
 <div class="border border-black p-3 mb-2">
     <h6 class="font-bold">COVID DISCLAIMER:</h6>
+    <p class="mb-4">
+        By registering for/attending this event, I acknowledge that I fully understand the nature and extent of the
+        risks presented by COVID-19 due to my in-person attendance at this event, including the risk that COVID19 may lead to severe illness or death. I also understand and acknowledge that there are risks of exposure
+        to COVID-19, whether resulting from travel through high-risk areas, the failure of other individuals to
+        follow proper COVID-19 protocols, such as maintaining proper social distancing and proper hygiene
+        measures, and other such risks. While I understand that CJMEA has taken reasonable steps to address the
+        risks presented by COVID-19, I recognize that the COVID-19 protocols being utilized at the event may be
+        insufficient to prevent my contracting COVID-19 and suffering any related injuries, and that I expressly
+        nevertheless assume all of these risks.
+    </p>
     <p>
-        By registering for/attending this event, I acknowledge that I fully understand the nature and extent of the risks
-        presentd by COVID-19 due to my in-person attendance at this event, including the risk that COVID-19 may lead to
-        severe illness or death.  I also understand and acknowledge that there are risks of exposure to COVID-19, whether
-        resulting from travel through high-risk areas, the failure of other individuals to follow proper COVID-19
-        protocols, such as maintaining proper social distancing and proper hygiene measures, and other risks.  While I
-        understand that CJMEA has taken reasonable steps to address the risks presented by COVID-19, I recognize that the
-        COVID-19 protocols being utilized at the event may be insufficient to prevent my contracting COVID-19 and suffering
-        any related injuries, and that I expressly waive, and discharge CJMEA, its officers, directors, employees, contractors,
-        and agents, from any and all liability, loss, damage, claims, demands, actions, and causes of action whatsoever,
-        including reasonable attorneys' fees, directly or indirectly arising out of or related to any loss, damage,
-        injury, or death, that may be sustained by me while participating in this event or while in, on, or around the
-        event premises that may lead to exposure or harm due to COVID-19.
+        With full knowledge of the risks involved, therefore, I hereby release, waive, and discharge CJMEA, its
+        officers, directors, employees, contractors, and agents, from any and all liability, loss, damage, claims,
+        demands, actions, and causes of action whatsoever, including reasonable attorneys' fees, directly or
+        indirectly arising out of or related to any loss, damage, injury, or death, that may be sustained by me
+        while participating in this event or while in, on, or around the event premises that may lead to exposure
+        or harm due to COVID-19.
     </p>
 </div>
