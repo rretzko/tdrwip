25b2c54fdf51ac5caa00c83d309f2c0b1a29dea1
Retzko <rick@mfrholdings.com>
2021-10-19 13:44:30 -0400

    Surpress paypal button

--- resources/views/registrants/estimateforms/12/66/show.blade.php
+++ resources/views/registrants/estimateforms/12/66/show.blade.php
@@ -33,13 +33,13 @@
                                 </a>
                             </div>
 
-                            {{-- PAYPAL BUTTON --}}
+                            {{-- PAYPAL BUTTON
                             <div class="bg-yellow-300 px-2 pt-3 rounded-2xl font-bold">
                                 <a href="{{ route('registrant.paypal') }}" class="bg-yellow-300 text-blue-700">
                                     Pay via PayPal
                                 </a>
                             </div>
-
+--}}
                             {{-- BUTTON TO DOWNLOAD PDF --}}
                             <div class="bg-blue-400 text-xs pt-3 border rounded-2xl text-white px-2">
                                 @if(config('app.url') === 'http://localhost')
