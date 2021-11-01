b22b1e216a2eeb66b5143c40eb6581eef052d370
MFRHoldings <rick@mfrholdings.com>
2021-11-01 11:50:27 -0400

    Add All-Shore invoice pdf download.

--- resources/views/pdfs/estimateforms/19/69/estimateform.blade.php
+++ resources/views/pdfs/estimateforms/19/69/estimateform.blade.php
@@ -15,162 +15,77 @@
 </head>
 <body style="border: 1px solid white; padding: 1rem; ">
 
+<table style="border-bottom: 1px solid darkgrey;">
+    <tr>
+        <th>{{ $eventversion->name }}</th>
+    </tr>
+</table>
+
 {{-- SEND TO INSTRUCTIONS --}}
 {{-- SEND TO --}}
-<table>
+<table style="text-align: center; border-bottom: 1px solid darkgrey;">
     <tr>
-        <td>The pages below should be sent to: </td>
+        <td>Sent to: Emily Kneuer, Treasurer</td>
     </tr>
     <tr>
         <td>
-            <b>{{ $sendto['name'] }}</b>
+            <b>Raritan High School</b>
         </td>
     </tr>
     <tr>
-        <td>{{ $sendto['address01'] }}</td>
+        <td>419 Middle Road</td>
     </tr>
     <tr>
-        <td>{{ $sendto['address02'] }}</td>
+        <td>Hazlet, NJ 07730</td>
     </tr>
-    @if(strlen($sendto['address03']))
-        <tr>
-            <td>{{ $sendto['address03'] }}</td>
-        </tr>
-    @endif
 </table>
 
-{{-- PAGE BREAK --}}
-<div class="page_break"></div>
-
-{{-- PAGE HEADER --}}
-<table>
+<table style="border-bottom: 1px solid darkgrey;">
     <tr>
-        <td style="width: 10%;">
-            <img src="https://thedirectorsroom.com/assets/images/njmea_logo_state.jpg" alt="NJMEA Logo"/>
+        <th>INVOICE</th>
+    </tr>
+    <tr>
+        <th>{{ $eventversion->name }}</th>
+    </tr>
+    <tr>
+        <th>STUDENT AUDITION FEE INVOICE</th>
+    </tr>
+    <tr>
+        <th style="padding: 1rem 0;">${{ number_format($eventversion->eventversionconfigs->registrationfee, 2) }} Per Student</th>
+    </tr>
+    <tr>
+       <th style="padding-bottom: 1rem;">DUE: November 3<sup>rd</sup>, 2021</th>
+    </tr>
+    <tr>
+        <th>Please send one check for all students auditioning at your school.<br />
+        Please make out all checks to "All-Shore Chorus Inc.</th>
+    </tr>
+</table>
+
+<table style="margin: auto;">
+    <tr>
+        <td style="width: 20rem;">School</td>
+        <th style="text-align: left;">{{ $school->name }}</th>
+    </tr>
+    <tr>
+        <td>Date</td>
+        <th style="text-align: left;" >{{ \Carbon\Carbon::now()->format('d-M-Y') }}</th>
+    </tr>
+    <tr>
+        <td>Number of Students Auditioning</td>
+        <th style="text-align: left;" >{{ $registrants->count() }}</th>
+    </tr>
+    <tr>
+        <td>Choral Director</td>
+        <th style="text-align: left;" >{{ auth()->user()->person->fullName }}</th>
+    </tr>
+    <tr>
+        <td>
+            Total amount enclosed: ({{ $registrants->count() }} of students x ${{ number_format($eventversion->eventversionconfigs->registrationfee, 2) }})
         </td>
-        <td style="width: 72%;text-align: right;">
-            <div style="font-weight: bold; text-align: right; ">{{ strtoupper($eventversion->name) }}</div>
-            <div style="text-align: right"><span style="border-top: 1px solid darkgrey;">2021-2022 TEACHER ESTIMATE FORM</span></div>
-            <div style="text-align: right; font-size: 1rem;">
-                {{ $me->person->fullName }}
-            </div>
-            <div>
-                {{ $school->shortName }}
-            </div>
-        </td>
-    </tr>
-</table>
-
-<table>
-    <tr>
-        <th style="text-align: center; border-bottom: 1px solid darkgrey;">{{ $eventversion->eventversionconfigs->max_count }} STUDENTS MAXIMUM</th>
-    </tr>
-</table>
-
-<style>#roster{font-size: .9rem;} #roster td,#roster th{border: 1px solid black; text-align: center;}</style>
-<table style="border-collapse: collapse;" id="roster">
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Voice Part</th>
-        <th>Grade</th>
-        <th>Fee</th>
-    </tr>
-    </thead>
-    <tbody>
-    @for($i=0;$i<$eventversion->eventversionconfigs->max_count;$i++)
-        @if(isset($registrants[$i]))
-            <tr>
-                <td>{{ ($i + 1) }}</td>
-                <td style="text-align: left;padding-left: .25rem;">{{ $registrants[$i]->student->person->fullNameAlpha }}</td>
-                <td>{{ $registrants[$i]->instrumentations->first()->descr }}</td>
-                <td>{{ $registrants[$i]->student->grade }}</td>
-                <td>${{ $eventversion->eventversionconfigs->registrationfee * ($i + 1) }}</td>
-            </tr>
-        @else
-            <tr>
-                <td>{{ ($i + 1) }}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>${{ $eventversion->eventversionconfigs->registrationfee * ($i + 1) }}</td>
-            </tr>
-        @endif
-
-    @endfor
-    </tbody>
-</table>
-
-{{-- SUMMARY TABLE --}}
-<style>
-    #summary td,#summary th{border: 1px solid black; text-align: center;}
-</style>
-{{-- SUMMARY TABLE --}}
-<table id="summary">
-    <thead>
-    <tr>
-        <th style="border-top: 0; border-left: 0; "></th>
-        @foreach($eventversion->instrumentations() AS $instrumentation)
-            <th >{{ strtoupper($instrumentation->abbr) }}</th>
-        @endforeach
-        <th>Total Enclosed</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Voice Part Totals</td>
-        @foreach($eventversion->instrumentations() AS $instrumentation)
-            <th style="{{ (! $registrantsbyinstrumentation[$instrumentation->id]) ? 'color:lightgrey;' : '' }}">
-                {{ $registrantsbyinstrumentation[$instrumentation->id] }}
-            </th>
-        @endforeach
-        <td style="text-align: center">${{ array_sum($registrantsbyinstrumentation) * $eventversion->eventversionconfigs->registrationfee }}</td>
-    </tr>
-    </tbody>
-</table>
-
-{{-- PAGE BREAK --}}
-<div class="page_break"></div>
-
-{{-- MEMBERSHIP CARD --}}
-<table>
-    <tr>
-        <td style="width: 10%; text-align: center;">
-            <img src="https://thedirectorsroom.com/assets/images/NAfME_cropped.jpg" width="600px" alt="NAfME Logo"/>
-        </td>
-    </tr>
-</table>
-
-<table>
-    <tr>
-        <td style="font-size: 1.5rem; font-weight: bold; text-align: center">
-            Please attach a copy of your current NAfME card here.<br />
-        Membership must be current through December 2021.
-        </td>
-    </tr>
-    <tr>
-        <td style="font-size: 1.25rem; font-weight: bold; text-align: center">
-            <span style="color: white">
-                Note: If you uploaded a copy of your membership card into
-                TheDirectorsRoom.com, it will be printed below.
-            </span>
-        </td>
-    </tr>
-</table>
-
-<table>
-    <tr>
-        <td style="font-size:12rem; color: transparent;">DELIBERATELY BLANK</td>
-    </tr>
-</table>
-
-<table>
-    <tr>
-        <td style="font-size: 1.5rem; font-weight: bold; text-align: center">
-            Please contact NAfME at 800.828.0229 or <br />
-            www.nafme.org for validation/renewal.
-        </td>
+        <th style="text-align: left;" >
+            ${{ number_format($registrants->count() * $eventversion->eventversionconfigs->registrationfee, 2) }}
+        </th>
     </tr>
 </table>
 
