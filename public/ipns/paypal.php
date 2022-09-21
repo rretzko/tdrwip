8efcc213c215122b6b7afe3962bdfe72185f2b47
MFRHoldings <rick@mfrholdings.com>
2022-09-21 12:08:42 -0400

    Test quick header in public.

--- public/ipns/paypal.php
+++ public/ipns/paypal.php
@@ -1,7 +1,7 @@
 <?php namespace Listener;
 
 //use App\Http\Controllers\Paypal\PayPalController;
-use Illuminate\Support\Facades\Log;
+use \Illuminate\Support\Facades\Log;
 
 Log::info(__FILE__.':'.date('Y-m-d G:i:s'));
 
