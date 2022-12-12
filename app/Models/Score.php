2e402a424185352419402e883ad28f4af50c6d18
MFRHoldings <rick@mfrholdings.com>
2022-12-12 11:17:31 -0500

    Added conditional status for no scores (ex. no-show) for CJMEA.

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -41,129 +41,134 @@ class Score extends Model
             $scoringcomponents[$score['scoringcomponent_id']][] = $score['score'];
         }
 
-        switch($this->eventversion->event->id){
-            case 1: //CJMEA
-                $scores =
-                    [
-                        $scoringcomponents[75][0], //scales quality
-                        $scoringcomponents[76][0], //scales diatonic intonation
-                        $scoringcomponents[77][0], //scales chromatic intonation
-                        $scoringcomponents[78][0], //solo quality
-                        $scoringcomponents[79][0], //solo intonation
-                        $scoringcomponents[80][0], //solo musicanship
-                        $scoringcomponents[81][0], //swan quality
-                        $scoringcomponents[82][0], //swan intonation
-                        $scoringcomponents[83][0], //swan musicianship
+        if($scores) {
+            switch ($this->eventversion->event->id) {
+                case 1: //CJMEA
+                    $scores =
+                        [
+                            $scoringcomponents[75][0], //scales quality
+                            $scoringcomponents[76][0], //scales diatonic intonation
+                            $scoringcomponents[77][0], //scales chromatic intonation
+                            $scoringcomponents[78][0], //solo quality
+                            $scoringcomponents[79][0], //solo intonation
+                            $scoringcomponents[80][0], //solo musicanship
+                            $scoringcomponents[81][0], //swan quality
+                            $scoringcomponents[82][0], //swan intonation
+                            $scoringcomponents[83][0], //swan musicianship
 
-                        $scoringcomponents[75][1],
-                        $scoringcomponents[76][1],
-                        $scoringcomponents[77][1],
-                        $scoringcomponents[78][1],
-                        $scoringcomponents[79][1],
-                        $scoringcomponents[80][1],
-                        $scoringcomponents[81][1],
-                        $scoringcomponents[82][1],
-                        $scoringcomponents[83][1],
-                    ];
-                break;
-            case 19: //All-Shore
+                            $scoringcomponents[75][1],
+                            $scoringcomponents[76][1],
+                            $scoringcomponents[77][1],
+                            $scoringcomponents[78][1],
+                            $scoringcomponents[79][1],
+                            $scoringcomponents[80][1],
+                            $scoringcomponents[81][1],
+                            $scoringcomponents[82][1],
+                            $scoringcomponents[83][1],
+                        ];
+                    break;
+                case 19: //All-Shore
 
-                if(isset($scoringcomponents)) {
+                    if (isset($scoringcomponents)) {
+                        $scores = [
+                            //All-Shore 2022-23
+                            $scoringcomponents[64][0], //low scale
+                            $scoringcomponents[65][0], //high scale
+                            $scoringcomponents[66][0], //chromatic scale
+                            $scoringcomponents[67][0], //arpeggio
+                            $scoringcomponents[69][0], //quartet
+                            $scoringcomponents[68][0], //solo
+
+                            $scoringcomponents[64][1],
+                            $scoringcomponents[65][1],
+                            $scoringcomponents[66][1],
+                            $scoringcomponents[67][1],
+                            $scoringcomponents[69][1],
+                            $scoringcomponents[68][1],
+
+                            $scoringcomponents[64][2],
+                            $scoringcomponents[65][2],
+                            $scoringcomponents[66][2],
+                            $scoringcomponents[67][2],
+                            $scoringcomponents[69][2],
+                            $scoringcomponents[68][2],
+                        ];
+                    } else {
+
+                        $scores = [
+                            0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0,
+                        ];
+                    }
+                    break;
+                case 25: //MAHC
                     $scores = [
-                        //All-Shore 2022-23
-                        $scoringcomponents[64][0], //low scale
-                        $scoringcomponents[65][0], //high scale
-                        $scoringcomponents[66][0], //chromatic scale
-                        $scoringcomponents[67][0], //arpeggio
-                        $scoringcomponents[69][0], //quartet
-                        $scoringcomponents[68][0], //solo
+                        //MAHC 2022-23
+                        $scoringcomponents[70][0], //low scale quality
+                        $scoringcomponents[71][0], //low scale intonation
+                        $scoringcomponents[72][0], //high scale quality
+                        $scoringcomponents[73][0], //high scale intonation
+                        $scoringcomponents[94][0], //solo quality
+                        $scoringcomponents[95][0], //solo intonation
+                        $scoringcomponents[96][0], //solo musicianship
 
-                        $scoringcomponents[64][1],
-                        $scoringcomponents[65][1],
-                        $scoringcomponents[66][1],
-                        $scoringcomponents[67][1],
-                        $scoringcomponents[69][1],
-                        $scoringcomponents[68][1],
+                        $scoringcomponents[70][1], //...
+                        $scoringcomponents[71][1],
+                        $scoringcomponents[72][1],
+                        $scoringcomponents[73][1],
+                        $scoringcomponents[94][1],
+                        $scoringcomponents[95][1],
+                        $scoringcomponents[96][1],
 
-                        $scoringcomponents[64][2],
-                        $scoringcomponents[65][2],
-                        $scoringcomponents[66][2],
-                        $scoringcomponents[67][2],
-                        $scoringcomponents[69][2],
-                        $scoringcomponents[68][2],
+                        $scoringcomponents[70][2],
+                        $scoringcomponents[71][2],
+                        $scoringcomponents[72][2],
+                        $scoringcomponents[73][2],
+                        $scoringcomponents[94][2],
+                        $scoringcomponents[95][2],
+                        $scoringcomponents[96][2],
                     ];
-                }else{
-
+                    break;
+                default: //NJ All-State
                     $scores = [
-                        0,0,0,0,0,0,
-                        0,0,0,0,0,0,
-                        0,0,0,0,0,0,
+                        $scoringcomponents[48][0], //judge 1, Scales Quality
+                        $scoringcomponents[49][0], //judge 1, Scales Low Scales
+                        $scoringcomponents[50][0], //judge 1, Scales High Scales
+                        $scoringcomponents[51][0], //judge 1, Scales Chromatic Scales
+                        $scoringcomponents[58][0], //judge 1, Solo Quality
+                        $scoringcomponents[59][0], //judge 1, Solo Intonation
+                        $scoringcomponents[60][0], //judge 1, Solo Musicianship
+                        $scoringcomponents[61][0], //judge 1, Quintet Quality
+                        $scoringcomponents[62][0], //judge 1, Quintet Intonation
+                        $scoringcomponents[63][0], //judge 1, Quintet Musicianship
+
+                        $scoringcomponents[48][1], //judge 2, Scales Quality
+                        $scoringcomponents[49][1], // etc.
+                        $scoringcomponents[50][1],
+                        $scoringcomponents[51][1],
+                        $scoringcomponents[58][1],
+                        $scoringcomponents[59][1],
+                        $scoringcomponents[60][1],
+                        $scoringcomponents[61][1],
+                        $scoringcomponents[62][1],
+                        $scoringcomponents[63][1],
+
+                        $scoringcomponents[48][2],
+                        $scoringcomponents[49][2],
+                        $scoringcomponents[50][2],
+                        $scoringcomponents[51][2],
+                        $scoringcomponents[58][2],
+                        $scoringcomponents[59][2],
+                        $scoringcomponents[60][2],
+                        $scoringcomponents[61][2],
+                        $scoringcomponents[62][2],
+                        $scoringcomponents[63][2],
                     ];
-                }
-                break;
-            case 25: //MAHC
-                $scores = [
-                    //MAHC 2022-23
-                    $scoringcomponents[70][0], //low scale quality
-                    $scoringcomponents[71][0], //low scale intonation
-                    $scoringcomponents[72][0], //high scale quality
-                    $scoringcomponents[73][0], //high scale intonation
-                    $scoringcomponents[94][0], //solo quality
-                    $scoringcomponents[95][0], //solo intonation
-                    $scoringcomponents[96][0], //solo musicianship
-
-                    $scoringcomponents[70][1], //...
-                    $scoringcomponents[71][1],
-                    $scoringcomponents[72][1],
-                    $scoringcomponents[73][1],
-                    $scoringcomponents[94][1],
-                    $scoringcomponents[95][1],
-                    $scoringcomponents[96][1],
-
-                    $scoringcomponents[70][2],
-                    $scoringcomponents[71][2],
-                    $scoringcomponents[72][2],
-                    $scoringcomponents[73][2],
-                    $scoringcomponents[94][2],
-                    $scoringcomponents[95][2],
-                    $scoringcomponents[96][2],
-                ];
-                break;
-            default: //NJ All-State
-                $scores = [
-                    $scoringcomponents[48][0], //judge 1, Scales Quality
-                    $scoringcomponents[49][0], //judge 1, Scales Low Scales
-                    $scoringcomponents[50][0], //judge 1, Scales High Scales
-                    $scoringcomponents[51][0], //judge 1, Scales Chromatic Scales
-                    $scoringcomponents[58][0], //judge 1, Solo Quality
-                    $scoringcomponents[59][0], //judge 1, Solo Intonation
-                    $scoringcomponents[60][0], //judge 1, Solo Musicianship
-                    $scoringcomponents[61][0], //judge 1, Quintet Quality
-                    $scoringcomponents[62][0], //judge 1, Quintet Intonation
-                    $scoringcomponents[63][0], //judge 1, Quintet Musicianship
-
-                    $scoringcomponents[48][1], //judge 2, Scales Quality
-                    $scoringcomponents[49][1], // etc.
-                    $scoringcomponents[50][1],
-                    $scoringcomponents[51][1],
-                    $scoringcomponents[58][1],
-                    $scoringcomponents[59][1],
-                    $scoringcomponents[60][1],
-                    $scoringcomponents[61][1],
-                    $scoringcomponents[62][1],
-                    $scoringcomponents[63][1],
-
-                    $scoringcomponents[48][2],
-                    $scoringcomponents[49][2],
-                    $scoringcomponents[50][2],
-                    $scoringcomponents[51][2],
-                    $scoringcomponents[58][2],
-                    $scoringcomponents[59][2],
-                    $scoringcomponents[60][2],
-                    $scoringcomponents[61][2],
-                    $scoringcomponents[62][2],
-                    $scoringcomponents[63][2],
-                ];
+            }
+        }else{ //no show
+            //hard-coded for cjmes
+            $scores = array_fill(0,18,0);
         }
 
         return $scores;
